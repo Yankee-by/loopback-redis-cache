ff1b41cbe835f889dd2beacbd11d4a1fcc8f2af6
Yury Grynko <yury.grynko@monterosa.co.uk>
2020-03-09 17:13:35 +0300

    HOTFIX: getPrefix function was fixed

--- utils.js
+++ utils.js
@@ -24,7 +24,7 @@ function getKeyPrefix({options, ctx}) {
     ctxParameterAsPrefix
   } = options;
 
-  return String( get(ctx, ctxParameterAsPrefix, customPrefix) );
+  return String( get(ctx, ctxParameterAsPrefix, customPrefix) || '' );
 }
 
 
